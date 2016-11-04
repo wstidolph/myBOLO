0d4450a537b7e258ad34a3e128df0777f8eb7a25
Wayne Stidolph <wayne@stidolph.com>
2016-11-04 12:32:09 -0700

    Bring in basic auth functions (verified 'serve' runs, haven't checked function)

---
+++ config/fb_conf.dev.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by wayne on 10/21/2016.
+ */
+export declare const firebaseConfig: {
+    apiKey: string;
+    authDomain: string;
+    databaseURL: string;
+    storageBucket: string;
+    messagingSenderId: string;
+};
+export declare const dt_firebaseConfig: {
+    apiKey: string;
+    authDomain: string;
+    databaseURL: string;
+    storageBucket: string;
+    messagingSenderId: string;
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,44 @@ import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { LoginPage } from '../pages/login/login';
+import { ResetPasswordPage } from '../pages/reset-password/reset-password';
+import { SignupPage } from '../pages/signup/signup';
+
+import { AuthData } from '../providers/auth-data';
+
+// Import the AF2 Module
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import {firebaseConfig} from '../../config/fb_conf.dev';
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+}
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    ResetPasswordPage,
+    SignupPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    ResetPasswordPage,
+    SignupPage
   ],
-  providers: []
+  providers: [
+    AuthData
+  ]
 })
 export class AppModule {}
+
